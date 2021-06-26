44ced960d44f8131a047c6a34ba9ffccaac8ac42
Oluwaseye <seyz4all@gmail.com>
2021-06-26 18:01:06 -0400

    Update minter.js

--- minter/minter.js
+++ minter/minter.js
@@ -4,29 +4,28 @@ const TruffleContract = require("truffle-contract");
 const Web3 = require("web3");
 require('dotenv').config();
 
-const KEY = process.env.KEY; //infura key
+const INFURA_KEY = process.env.INFURA_KEY;
 
-const OWNER_ACCOUNT = "0x5CD32Dc7583D54710869F241C59B7dae1E8e4D24";
-const MNEMONIC =
-  "swamp thank festival palm ridge run kick host label caution major squirrel";
+const OWNER_ACCOUNT = process.env.ACCOUNT;
+const MNEMONIC = process.env.MNEMONIC;
 
 const A = [
-  "0x1ba0df5159c4c75da8a30d34e28b0a2242b9634aed77c9b41b979e6081ed5033",
-  "0x04a81e18c8c57362b000213bce6d533055ba4f830dc76abf9c5bf37907ffbdd0",
+      "0x0897c412cc40f8677cdce4f903121daf686e8f97235133b0c2cb7b496646bf9e",
+      "0x1e46d176d6864c4bce9bd557c09e6a7dbd7e890b8f63d349aef2e3e0e7910cf7"
 ];
 const B = [
-  [
-    "0x272c1132c59a11b904df2e3921eaf7b40ce948a1a24e9b36dd6e2e04cc3e9560",
-    "0x1535e1e6c5cb4d685ef68595487910d68d8813765f422b977b53e32f8c53fc94",
-  ],
-  [
-    "0x26e8a26d9bd754c038c42bb9b5b32b91a0c1463aba53b03eb8e224f1230f853a",
-    "0x2c080f65faca972f26229da56b338fc12d62261f8626ec42659bc1090e7a983d",
-  ],
+      [
+        "0x2ac14c256b08b113ea51f507c8b3933297f6251f715e9a6a2a3660188e4cebd4",
+        "0x0ce6895d8dbec0bb77c6f32932f814c80108aeb91b98bc3fb695483eb2dc75ad"
+      ],
+      [
+        "0x2ec853923a212ea1bb0b7f674829e681a4e9df29cda08d1111d36888d75bda27",
+        "0x2ef9a38894a052093a1d70b7a17c00457cc28ad59880e18ba64252bba2280be2"
+      ]
 ];
 const C = [
-  "0x08c833d09a989255fa84bd16e9b4374fbf2c59f92f8b67298771b72c03e56f7f",
-  "0x2f85944aef8c9f217463077e0d8f85fdf5546b3b570820ade0cf9c95a3feb440",
+      "0x01d6b9b903610fcf2a4a6a19237b9d60a0d7aec0ceb3a418dd58486b44eac91f",
+      "0x1e651f82b48f5c2d65e27b9d41803c11ef45a7faa0beaf6dc969ee89621eec88"
 ];
 const INPUT = [
   "0x0000000000000000000000000000000000000000000000000000000000000009",
@@ -36,7 +35,7 @@ const INPUT = [
 (async () => {
   let provider = new HDWalletProvider(
     MNEMONIC,
-    `https://rinkeby.infura.io/v3/${KEY}`
+    `https://rinkeby.infura.io/v3/${INFURA_KEY}`
   );
   let web3 = new Web3(provider);
   let contractAbi = JSON.parse(
